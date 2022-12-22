a2de6830d683a3b32c965d9cec72b07b4711c79b
up-shang <943101718@qq.com>
2022-12-22 10:38:21 +0800

    feat: 300

---
+++ 300.最长递增子序列.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  //1 dp解法
+  // let len = nums.length
+  // if (len === 0) {
+  //   return 0
+  // }
+  // let dp = Array(len).fill(1)
+  // // 循环遍历找出最长递增数列，每次j+1统计
+  // for (let i = 0; i < len; i++) {
+  //   for (let j = 0; j < i; j++) {
+  //     if (nums[i] > nums[j]) {
+  //       dp[i] = Math.max(dp[i], dp[j] + 1)
+  //     }
+  //   }
+  // }
+  // return Math.max(...dp)
+
+  //2贪心算法 + 二分查找
+  // 让序列增长变慢
+  // 比如 1，2，3 好于 1，3，5
+  let len = nums.length
+  if (len === 0) {
+    return 0
+  }
+
+  let arr = [nums[0]]
+
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > arr[arr.length - 1]) {
+      arr.push(nums[i])
+    } else {
+      // 找到第一个nums[i]大于的值，替换为nums[i]
+      let left = 0
+      let right = arr.length - 1
+      while (left < right) {
+        // left与right一直动态变化
+        let mid = Math.floor((left + right) / 2)
+        if (arr[mid] < nums[i]) {
+          left = mid + 1
+        } else {
+          right = mid
+        }
+      }
+      // 替换为大数后，后续一级循环则可以生成更长的子序列
+      arr[left] = nums[i]
+    }
+  }
+  return arr.length
+};
+// @lc code=end
+
