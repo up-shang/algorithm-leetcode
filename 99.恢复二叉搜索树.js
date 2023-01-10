16008676bc1a56dc22e49a62c62dd6d151cf60e7
up-shang <943101718@qq.com>
2023-01-10 13:48:48 +0800

    chore: 优化注释说明

--- 99.恢复二叉搜索树.js
+++ 99.恢复二叉搜索树.js
@@ -21,6 +21,7 @@ var recoverTree = function (root) {
   let arr = []
   let first
   let second
+  // 采用中序遍历方式，拿到递归后的“递增数组”
   function dfs(node) {
     if (!node) return
     dfs(node.left)
@@ -29,10 +30,12 @@ var recoverTree = function (root) {
     dfs(node.right)
   }
   dfs(root)
-  // 注意题干，交换两个值，所以遍历数组查找下
+  // 注意题干，交换两个值，所以遍历数组查找下,数组正常情况为递增，现在必然有两处arr[i]>arr[i+1]的情况
+  // 故first取第一次发现的arr[i]
+  // second取第二次发现的小的值，arr[i+1]
   for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i].val > arr[i + 1].val) {
-      //第一次遇见，后续不再更新first ？？？？？
+      //第一次遇见的较大值，后续不再更新first
       if (!first) {
         first = arr[i]
       }
@@ -40,6 +43,7 @@ var recoverTree = function (root) {
       second = arr[i + 1]
     }
   }
+  // 对象引用类型，替换first与second后，表示替换了root内节点
   let tmp = first.val
   first.val = second.val
   second.val = tmp
