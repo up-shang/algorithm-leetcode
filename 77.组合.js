e4949de94d19336afb6e6d97d3fd7b42142ea096
up-shang <943101718@qq.com>
2023-01-30 15:30:37 +0800

    chore: 注释补充

--- 77.组合.js
+++ 77.组合.js
@@ -22,8 +22,10 @@ var combine = function (n, k) {
       ret.push([...path])
       return
     }
-    // i从1开始，
+    // i从1开始，j随着len逐渐后移
     for (let j = i; j <= n - k + len + 1; j++) {
+      // 先从1开始，弹出12 13 14后将1弹出，push进2
+      // 再弹出23 24，再推入3，弹出34，结束
       path.push(j)
       backtrack(n, k, j + 1)
       path.pop()
