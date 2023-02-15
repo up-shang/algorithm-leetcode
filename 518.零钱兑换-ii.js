209d3bd3dc047643fc7791950b49c7ad91496629
up-shang <943101718@qq.com>
2023-02-15 10:57:21 +0800

    chore: 优化注释

--- 518.零钱兑换-ii.js
+++ 518.零钱兑换-ii.js
@@ -12,12 +12,12 @@
  */
 var change = function (amount, coins) {
   let dp = new Array(amount + 1).fill(0)
-  dp[0] = 1
+  dp[0] = 1 // 起点为amount0,故组合为1
   for (let i = 0; i < coins.length; i++) {
     for (let j = coins[i]; j <= amount; j++) {
       // 先固定一个硬币
-      // 递推出每个j与amount之间的值的个数
-      // amount时就dp[j - coins[i]] + 当前
+      // 递推出每个j与amount之间的组合的个数
+      // amount时就dp[j - coins[i]]总和 + 当前coins
       dp[j] += dp[j - coins[i]]
     }
   }
