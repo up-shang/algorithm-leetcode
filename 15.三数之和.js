b14c22298aa1db9e1a43679ce4ba0369320881d0
up-shang <943101718@qq.com>
2023-02-23 19:59:49 +0800

    feat: 双指针或dfs 15 18

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -9,30 +9,70 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-  if(nums.length < 3) {
+var threeSum = function (nums) {
+  // 注释参见四数之和
+  if (nums.length < 3) {
+    return []
+  }
+  const list = []
+  const tmp = []
+  const len = nums.length
+  nums.sort((a, b) => a - b)
+  dfs(0, 3, 0)
+  return list
+
+  function dfs(index, count, target) {
+    if (count === 0 && target === 0) {
+      list.push([...tmp])
+      return
+    }
+    if (len - index < count) {
+      return
+    }
+    if (target > nums[len - 1] * count) {
+      return
+    }
+    if (target < nums[index] * count) {
+      return
+    }
+    for (let i = index; i < len; i++) {
+      if (i > index && nums[i] === nums[i - 1]) {
+        continue
+      }
+      tmp.push(nums[i])
+      dfs(i + 1, count - 1, target - nums[i])
+      tmp.pop()
+    }
+  }
+};
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum1 = function (nums) {
+  if (nums.length < 3) {
     return []
   }
   let list = []
-  nums.sort((a,b)=> a-b)
-  for(let i=0; i<nums.length-2; i++) {
-    if(nums[i] === nums[i-1]) {
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] === nums[i - 1]) {
       continue
     }
-    let left = i+1
-    let rigth = nums.length-1
-    while(left < rigth) {
-      if(nums[i] + nums[left] + nums[rigth] === 0) {
+    let left = i + 1
+    let rigth = nums.length - 1
+    while (left < rigth) {
+      if (nums[i] + nums[left] + nums[rigth] === 0) {
         list.push([nums[i], nums[left], nums[rigth]])
-        while(nums[left] === nums[left+1]) {
+        while (nums[left] === nums[left + 1]) {
           left++
         }
-        while(nums[rigth] === nums[rigth-1]) {
+        while (nums[rigth] === nums[rigth - 1]) {
           rigth--
         }
         left++
         rigth--
-      } else if(nums[i] + nums[left] + nums[rigth] > 0) {
+      } else if (nums[i] + nums[left] + nums[rigth] > 0) {
         rigth--
       } else {
         left++
---
+++ 18.四数之和.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=18 lang=javascript
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+  // 考虑使用回溯剪枝来解
+  const list = []
+  const tmp = []
+  const len = nums.length
+  nums.sort((a, b) => a - b)
+  dfs(0, 4, target)
+  return list
+  /**
+   * 
+   * @param {number} index 当前索引
+   * @param {number} count 匹配的数量
+   * @param {number} target 目标值
+   */
+  function dfs(index, count, target) {
+    if (count === 0 && target === 0) {
+      // 注意此处不能直接push tmp ，注意每次tmp都会完全pop清空
+      list.push([...tmp])
+      return
+    }
+    // 剪枝，优化复杂度
+    // 如果剩余遍历数已不足count，直接退出
+    if (len - index < count) {
+      return
+    }
+    // 比最小还小，不满足
+    if (target < nums[index] * count) {
+      return
+    }
+    // 比最大还大，不满足
+    if (target > nums[len - 1] * count) {
+      return
+    }
+
+    // 每次从index开始，查找到不一样的结果集
+    for (let i = index; i < len; i++) {
+      // 跳过相同的值
+      if (i > index && nums[i] === nums[i - 1]) {
+        continue
+      }
+      tmp.push(nums[i])
+      // 下一次递归
+      dfs(i + 1, count - 1, target - nums[i])
+      // 由底层逐个推出tmp内的4个值
+      tmp.pop()
+    }
+  }
+};
+// @lc code=end
+
